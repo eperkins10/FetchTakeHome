96a86769e3bc4680d372c629489d8df9045117d4
Ethan Perkins <ethanperkinspro@gmail.com>
2024-08-15 16:44:50 -0700

    Initial Commit

---
+++ FetchTakeHome/Views/DessertView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FetchTakeHome
+//
+//  Created by Ethan Perkins on 8/15/24.
+//
+
+import SwiftUI
+
+struct DessertView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    DessertView()
+}
---
+++ FetchTakeHome/Views/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Ethan Perkins on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DessertView()
+        }
+    }
+}
